59ed83b88ffd77b7eb4c6a525b59d688a2f9b3ac
Kenny Peng <kennypeng515@gmail.com>
2023-11-16 14:36:58 -0800

    scaffolding for part 2

---
+++ transformed_data_migrator.py
@@ -0,0 +1,13 @@
+import boto3
+import pandas as pd
+from decimal import Decimal
+from dotenv import load_dotenv
+
+# TODO:
+# load dotenv vars
+
+# prompt user to input csv, read it in
+
+# connect to boto3
+
+# iterate throuogh csv and publish to dynamo
