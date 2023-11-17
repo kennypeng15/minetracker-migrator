395f9dbbdddcd904682ba47c93ea890def736bcd
Kenny Peng <kennypeng515@gmail.com>
2023-11-16 16:58:03 -0800

    migration code

--- transformed_data_migrator.py
+++ transformed_data_migrator.py
@@ -1,13 +1,87 @@
 import boto3
-import pandas as pd
+import os
+from os.path import join, dirname, isfile
+import csv
+import sys
 from decimal import Decimal
 from dotenv import load_dotenv
 
 # TODO:
-# load dotenv vars
+# load necessary environment variables
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 # prompt user to input csv, read it in
-
-# connect to boto3
+while(True):
+    csv_name = input("Please input the path to a CSV file with transformed minesweeper game data: ")
+    if isfile(csv_name):
+        break
+    else:
+        print("Invalid file name, please try again.") 
 
 # iterate throuogh csv and publish to dynamo
+with open(csv_name, newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    # display how many rows, and ask for confirmation
+    row_count = sum(1 for row in reader)
+    confirmation = input("There are " + str(row_count) + " rows in the CSV. Continue? (y/n): ")
+    if confirmation.strip().lower() != "y":
+        print("Confirmation was not supplied. Terminating.")
+        sys.exit(0)
+    else:
+        print("Confirmation was supplied. Proceeding.")
+
+# connect to boto3
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(os.environ['DYNAMO_NAME'])
+
+# actually publish
+with open(csv_name, newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        # read in as correct types
+        game_id = str(row["game-id"])
+        game_timestamp = str(row["game-timestamp"])
+        difficulty = str(row["difficulty"])
+        elapsed_time = Decimal(row["elapsed-time"])
+        estimated_time = Decimal(row["estimated-time"])
+        board_solved = row["board-solved"].lower() == "true" # DO NOT use bool(row["board-solved"]), non-empty strings are truthy!
+        completed_3bv = int(row["completed-3bv"])
+        board_3bv = int(row["board-3bv"])
+        game_3bvps = Decimal(row["game-3bvps"])
+        useful_clicks = int(row["useful-clicks"])
+        wasted_clicks = int(row["wasted-clicks"])
+        total_clicks = int(row["total-clicks"])
+        efficiency = Decimal(row["efficiency"])
+        solve_percentage = Decimal(row["solve-percentage"])
+
+        # print(row)
+        # print(game_id, game_timestamp, difficulty)
+        # print(elapsed_time, estimated_time)
+        # print(board_solved)
+        # print(completed_3bv, board_3bv, game_3bvps)
+        # print(useful_clicks, wasted_clicks, total_clicks)
+        # print(efficiency, solve_percentage)
+        # print("---")
+
+# comment this out when you actually want to do the migration job!
+'''
+        table.put_item(
+            Item={
+                "game-id": game_id,
+                "game-timestamp": game_timestamp,
+                "difficulty": difficulty,
+                "elapsed-time": elapsed_time,
+                "estimated-time": estimated_time,
+                "board-solved": board_solved,
+                "completed-3bv": completed_3bv,
+                "board-3bv": board_3bv,
+                "game-3bvps": game_3bvps,
+                "useful-clicks": useful_clicks,
+                "wasted-clicks": wasted_clicks,
+                "total-clicks": total_clicks,
+                "efficiency": efficiency,
+                "solve-percentage": solve_percentage
+            }
+        )
+'''
